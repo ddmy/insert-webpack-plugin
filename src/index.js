8bd191466c41233256180a8d677450826162789c
YiJio <841312998@qq.com>
2022-02-10 16:01:31 +0800

    insertPosition

--- src/index.js
+++ src/index.js
@@ -1,6 +1,35 @@
 function InsertPlugin(options) {
   this.pushArr = options.inner || []
   this.type = options.type || /.*/
+  // before after function (source) {}
+  this.insertPosition = options.insertPosition || 'before'
+}
+
+InsertPlugin.prototype.computedSource = function (assets, filename) {
+  if (typeof assets[filename].source === 'function') {
+    let source = assets[filename].source()
+    let size = assets[filename].size()
+    if (this.insertPosition === 'before') {
+      source = this.pushArr.join('') + source
+      size = this.pushArr.join('').length + size
+    } else if (this.insertPosition === 'after') {
+      source = source + this.pushArr.join('')
+      size = this.pushArr.join('').length + size
+    } else if (typeof this.insertPosition === 'function') {
+      source = this.insertPosition(source)
+      size = source.length
+    }
+    assets[filename].source = () => source
+    assets[filename].size = () => size
+  } else if (assets[filename]._source && assets[filename]._source.children) {
+    if (this.insertPosition === 'before') {
+      assets[filename]._source.children = this.pushArr.concat(assets[filename]._source.children)
+    } else if (this.insertPosition === 'after') {
+      assets[filename]._source.children = assets[filename]._source.children.concat(this.pushArr)
+    } else if (typeof this.insertPosition === 'function') {
+      assets[filename]._source.children = this.insertPosition(assets[filename]._source.children)
+    }
+  }
 }
 
 InsertPlugin.prototype.apply = function(compiler) {
@@ -12,14 +41,7 @@ InsertPlugin.prototype.apply = function(compiler) {
     for (var filename in compilation.assets) {
       // 处理文件范围判断
       if ((type === 'object' && this.type.test(filename)) || (type === 'string' && filename === type)) {
-        if (typeof compilation.assets[filename].source === 'function') {
-          const source = this.pushArr.join('') + compilation.assets[filename].source()
-          const size = this.pushArr.join('').length + compilation.assets[filename].size()
-          compilation.assets[filename].source = () => source
-          compilation.assets[filename].size = () => size
-        } else if (compilation.assets[filename]._source && compilation.assets[filename]._source.children){
-          compilation.assets[filename]._source.children = this.pushArr.concat(compilation.assets[filename]._source.children)
-        }
+        this.computedSource(compilation.assets, filename)
       }
     }
     callback()
